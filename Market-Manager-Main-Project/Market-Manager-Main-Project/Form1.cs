ee2fa47949be25a8dae3a85216c72226d5c536ff
Salam Al Hasan <selamalhasan@gmail.com>
2020-12-21 01:32:46 +0300

    Adding new comment

--- Market-Manager-Main-Project/Market-Manager-Main-Project/Form1.cs
+++ Market-Manager-Main-Project/Market-Manager-Main-Project/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace market_manager
-{
+{//Merhaba Arkadaslar
     public partial class Form1 : Form
     {
         Form2 f2 = new Form2();
